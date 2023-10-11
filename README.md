43280051e03f0be7086f20194b0756b6c9910532
Za Matheson <zamatheson@Zas-MacBook-Air.local>
2023-10-11 14:40:16 -0500

    added local storage and readme. fixed up some javascript and html

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Daily-Work-Planner
+
+This application is a work scheduler that can be used to as a planner to lay out the day by the hour. This application shows color-coded textboxes for the user to submit a message into, these textboxes are coded according to the time. If the textbox is in the past it will appear gray, if the textbox is in the current hour it will be red, and if the textbox is in the future it will appear green. Example: it is currently 12 pm-- the textboxes for 9, 10 and 11 am appear gray, the 12 pm timebox is red, and 1, 2, 3, 4, 5 pm are all green. When the user submits their message with the submit button, it is then logged into local stoage to be accessed when necessary.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,34 @@
+// date and time variables.
 var currentDate = new Date();
+var currentHour = currentDate.getHours();
 
 var currentYear = currentDate.getFullYear();
 var currentMonth = currentDate.getMonth();
 var currentDay = currentDate.getDate();
 
+// display current date and time at the top of the page.
 document.getElementById("currentDayTime").innerHTML = currentDate
 
 
+// display color-coded timeblocks according to past, present and future times.
 document.querySelectorAll(".timeblock").forEach(timeblock => {
   const hour = parseInt(timeblock.id.split("-")[1]);
   const timeblockDate = new Date(currentYear, currentMonth, currentDay, hour, 0, 0);
 
 if(timeblockDate < currentDate) {
-  timeblock.style.backgroundColor = "#d3d3d3";
+  $(timeblock).addClass("past");
 } else if (timeblockDate.getHours() === currentHour) {
-  timeblock.style.backgroundColor = "#ff6961";
+  $(timeblock).addClass("present");
 } else {
-  timeblock.style.backgroundColor = "#77dd77";
+  $(timeblock).addClass("future");
 }
 });
 
-$(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+// save text input into local stoage to manage schedule. 
+const saveButton = document.querySelector(".saveBtn");
+
+saveButton.addEventListener("click", function() {
+  const scheduleMsg = document.querySelector(".textBox").value;
+  localStorage.setItem("scheduleMsg", scheduleMsg);
+  alert("Scheduled Saved");
+})
